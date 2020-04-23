724db0aae0f78594521d02fcc280a46d3667e3a2
Christophe Chazeau <christophe.chazeau@gmail.com>
2020-04-23 16:52:47 +0200

    Forgot to add the tests

--- tests/examples.py
+++ tests/examples.py
@@ -1,5 +1,6 @@
 """
 Test / Example file for OpenDistro Secuity API
+TODO : Implement with unittest
 """
 import os
 import sys
@@ -10,56 +11,60 @@ from tenants import *
 import json
 from pprint import pprint
 
-od = OpenDistro(host="elastique-cherche.chazeau.org",port=443 ,user="admin", pwd="admin")
+#Create OpenDistro Connection
+od = OpenDistro(host="elastique-cherche.chazeau.org",
+                port=443 ,
+                user="admin", 
+                pwd="admin")
 
-print(">>> We are Connected ..." if od.check_connection() else "Problem with the connecetion")
+#Check the connection
+if (od.check_connection()):
+    print(">>> We are Connected ...")
+else: 
+    print(f"Problem with connecting to {od.host}:{od.port} with user {od.user}")
+    exit(1)
 
+#Create a Tenant Client for direct lowlevel objects creations
 tenants_client = TenantsClient(od)
-TEST_TENANT = "test_tenant"
+test_tenant_name_lowlevel = "_test_tenant_lowlevel"
+test_tenant_name_highlevel = "_test_tenant_highwlevel"
+test_tenant_name_highlevel_updated = "_updated_test_tenant_highlevel"
 
-print(">>> Creating tenant from within the body")
-tenants_client.create_tenant(tenant=TEST_TENANT,body='{"description":"description test"}')
+print(">>> Creating a tenant with the lowlevel methods")
+tenants_client.create_tenant(tenant=test_tenant_name_lowlevel,body='{"description":"This tenant was created for testing purpose with the lowlevel API"}')
 
-print(">>> Creating tenant by building a Tenant Object")
-tenant_dict = { "Tenant Creatd with an object" : 
-                { "description":"Tenant Object description",
-                    "hidden":False,
-                    "static":False,
-                    "reserved":False 
-                } 
-               }
-
-tenant_name = list(tenant_dict.keys())[0]
-tenant_description  = tenant_dict[tenant_name]["description"]
-tenant_static= tenant_dict[tenant_name]["static"]
-tenant_hidden = tenant_dict[tenant_name]["hidden"]
-tenant_reserved = tenant_dict[tenant_name]["reserved"]
-
-tenant_object = OpenDistroTenant(name=tenant_name,
-                                 description=tenant_description,
-                                 hidden=tenant_hidden,
-                                 static=tenant_static,
-                                 reserved=tenant_reserved)
+print(">>> Creating tenant with Objects (High level API)")
+tenant_object = OpenDistroTenant(name=test_tenant_name_highlevel,
+                                 description="This tenant was created for testing purposes with the High Level API")
+print(">>> Display the created tenant :")
 tenant_object.display()
-print(">>> Updating this tenant name")
-tenant_object.description = "Updated Tenant Description"
-tenant_object.name = "Updated Tenant Name"
+print(">>> Saving the created tenant to OpenDistro")
+tenant_object.save(tenants_client)
+print(">>> Updating this tenant's name and decription")
+tenant_object.description = "This tenant was created for testing purposes with the High Level API - Updated"
+tenant_object.name = test_tenant_name_highlevel_updated 
+tenant_object.save(tenants_client)
+print(">>> Display the updatedtenant :")
 tenant_object.display()
 
-print(">>> Listing tenants :")
+print(">>> Listing tenants from the server:")
+created_tenant_sets = ( test_tenant_name_lowlevel, test_tenant_name_highlevel, test_tenant_name_highlevel_updated ) 
 tenants_dict = tenants_client.get_tenants()
-pprint(tenants_dict)
+if all (tenant in tenants_dict for tenant in created_tenant_sets):
+    print("     >>> Success : All created tenants found")
+else:
+    print("     >>> Error : Not found our tenants :(")
+print(">>> Print every tenant we find : ")
+pprint(tenants_dict.keys())
 
-print(f">>> Getting {TEST_TENANT}")
-tenant_dict = tenants_client.get_tenant(TEST_TENANT)
-print(f"--------- {tenant_dict}")
-tenant = OpenDistroTenant.fromdict(tenant_dict)
-tenant.display()
+print(">>> Deleting created test tenants (with low level api)")
+[ tenants_client.delete_tenant(tenant) for tenant in created_tenant_sets ]
 
-
-print(">>> Deleting tenant ")
-tenants_client.delete_tenant(TEST_TENANT)
-
-print(">>> Listing tenants :")
+print(">>> Checking that everything has been deleted")
 tenants_dict = tenants_client.get_tenants()
-pprint(tenants_dict)
+if any(tenant in tenants_dict for tenant in created_tenant_sets):
+    print("     >>> Error : A test tenant has not been deletedi :(")
+else:
+    print("     >>> Success : Not found any of ourtenants :)")
+print(">>> Print every tenant we find : ")
+pprint(tenants_dict.keys())
